fbe527ceabc7384f235ab699cccbaea8336663c9
Tabishayan <143871048+Tabishayan@users.noreply.github.com>
2023-10-08 11:20:38 +0500

    Delete Q35.ts

--- Q35.ts
+++
@@ -1,7 +0,0 @@
-const animalsWithCommonCharacteristic: string[] = ['dog', 'cat', 'rabbit'];
-
-for (const animal of animalsWithCommonCharacteristic) {
-  console.log(`A ${animal} would make a great pet.`);
-}
-
-console.log("Any of these animals would make a great pet!");

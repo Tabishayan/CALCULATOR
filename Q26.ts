eb349bfeb08288ea3a1401129a42f5bb335c1fc1
Tabishayan <143871048+Tabishayan@users.noreply.github.com>
2023-10-08 10:39:35 +0500

    Delete Q26.ts

--- Q26.ts
+++
@@ -1,7 +0,0 @@
-const aliencolor: string = 'green';
-
-if (alien_color === 'green') {
-  console.log("Player just earned 5 points for shooting the green alien.");
-} else {
-  console.log("Player just earned 10 points.");
-}

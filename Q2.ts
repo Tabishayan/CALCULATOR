63df7eb481d6f7c9cc7ea7ada2e4550be221a0a2
Tabishayan <143871048+Tabishayan@users.noreply.github.com>
2023-10-08 10:36:43 +0500

    Delete Q2.ts

--- Q2.ts
+++
@@ -1,7 +0,0 @@
-var personName = "Tabish";
-
-console.log("Lowercase: " + personName.toLowerCase());
-
-console.log("Uppercase: " + personName.toUpperCase());
-
-console.log("Titlecase: " + personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' '));

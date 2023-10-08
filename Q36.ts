f10c769ead4917387845e4f3d9b51e377a55ddd2
Tabishayan <143871048+Tabishayan@users.noreply.github.com>
2023-10-08 11:20:56 +0500

    Delete Q36.ts

--- Q36.ts
+++
@@ -1,6 +0,0 @@
-function make_shirt(size: string, message: string): void {
-    console.log(`You've ordered a ${size}-sized shirt with the message: "${message}".`);
-  }
-  
-  make_shirt('medium', 'Hello World!');
-  
